f30297a5b6b1db9e212a0ba484d9d5d6b872a920
federicosimone <federico.simone15@gmail.com>
2025-11-10 23:10:20 +0100

    fine esercizio

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-let button = document.querySelector('#bottone') ;
+//preparazione dati
+let button = document.querySelector('#bottone')
+
+
+
+//gestione evento
 
 button.addEventListener("click", function() {
-    console.log("tasto cliccato");
-    document.querySelector('#lamp').src= "./img/yellow_lamp.png" ;
-    document.querySelector('#bottone').value= "Spegni";
-});
+    let img = document.querySelector('#lamp') ;
+    let button = document.querySelector('#bottone') ;
+    //img.src= "./img/yellow_lamp.png" ;
+    //button.innerHTML= "Spegni";
+    //console.log(img.src);
+
+    if ( img.src.includes("white_lamp.png")) {
+        img.src= "./img/yellow_lamp.png" ;
+        button.innerHTML= "Spegni";
+
+    } else {
+        img.src= "./img/white_lamp.png" ;
+        button.innerHTML= "Accendi";
+    }
+    
+}) ;
+
+
 
