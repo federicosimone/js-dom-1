115d77f12ca6ffaf2a24d0c1770559c575db5163
federicosimone <federico.simone15@gmail.com>
2025-11-11 00:19:47 +0100

    esperimenti

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let button = document.querySelector('#bottone') ;
 button.addEventListener("click", function() {
     let img = document.querySelector('#lamp') ;
     let button = document.querySelector('#bottone') ;
+    let paragrafo = document.querySelector('#buio') ;
     //img.src= "./img/yellow_lamp.png" ;
     //button.innerHTML= "Spegni";
     //console.log(img.src);
@@ -17,11 +18,13 @@ button.addEventListener("click", function() {
     if ( img.src.includes("white_lamp.png")) {
         img.src= "./img/yellow_lamp.png" ;
         button.innerHTML= "Spegni";
-        button.classList.toggle("btn-danger")
+        button.classList.toggle("btn-danger") ;
+        paragrafo.innerHTML="E luce fu";
     } else {
         img.src= "./img/white_lamp.png" ;
         button.innerHTML= "Accendi";
-        button.classList.toggle("btn-danger")
+        button.classList.toggle("btn-danger") ;
+        paragrafo.innerHTML="E' tutto buio";
         
     }
     
